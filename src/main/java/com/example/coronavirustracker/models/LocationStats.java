1faf71777d6b86cda705569789b60520b306401d
edwinandrew <edwinandrew96@gmail.com>
2020-09-17 17:38:07 -0400

    initial commit

---
+++ src/main/java/com/example/coronavirustracker/models/LocationStats.java
@@ -0,0 +1,50 @@
+package com.example.coronavirustracker.models;
+
+public class LocationStats {
+    private String state;
+    private String Country;
+    private int latestVirusTotalCases;
+
+    public int getDiffFromPrevDay() {
+        return diffFromPrevDay;
+    }
+
+    public void setDiffFromPrevDay(int diffFromPrevDay) {
+        this.diffFromPrevDay = diffFromPrevDay;
+    }
+
+    private int diffFromPrevDay;
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return Country;
+    }
+
+    public void setCountry(String country) {
+        Country = country;
+    }
+
+    public int getLatestVirusTotalCases() {
+        return latestVirusTotalCases;
+    }
+
+    public void setLatestVirusTotalCases(int latestVirusTotalCases) {
+        this.latestVirusTotalCases = latestVirusTotalCases;
+    }
+
+    @Override
+    public String toString() {
+        return "LocationStats{" +
+                "state='" + state + '\'' +
+                ", Country='" + Country + '\'' +
+                ", latestVirusTotalCases=" + latestVirusTotalCases +
+                '}';
+    }
+}
